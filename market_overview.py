8a112a0c878da3435867723b25ff600e426776bc
Emile Houvenaghel <emile.houvenaghel@telenet.be>
2021-04-20 23:13:05 +0200

    add headless tag to firefox driver

--- market_overview.py
+++ market_overview.py
@@ -20,15 +20,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 ###################################################################################################################
-CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/bin/chromedriver')
-GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome-stable')
+# CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/bin/chromedriver')
+# GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome-stable')
 
 url = 'https://finviz.com/futures.ashx'
-# options = webdriver.ChromeOptions()
+options = webdriver.FirefoxOptions()
 # options.binary_location = GOOGLE_CHROME_BIN
 options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
-options.add_argument('--disable-gpu')
+# options.add_argument('--disable-gpu')
 # options.add_argument('--no-sandbox')
 # options.add_argument("--disable-extensions")
 # options.add_argument('disable-infobars')
