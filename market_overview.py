140d8aac5c4e78c1179888366853af46a8bd2bda
Emile Houvenaghel <emile.houvenaghel@telenet.be>
2021-04-20 23:07:36 +0200

    add headless tag to firefox driver

--- market_overview.py
+++ market_overview.py
@@ -26,9 +26,9 @@ GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome-
 url = 'https://finviz.com/futures.ashx'
 # options = webdriver.ChromeOptions()
 # options.binary_location = GOOGLE_CHROME_BIN
-# options.add_argument("--headless")
-# options.add_argument("--window-size=1920,1080")
-# options.add_argument('--disable-gpu')
+options.add_argument("--headless")
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--disable-gpu')
 # options.add_argument('--no-sandbox')
 # options.add_argument("--disable-extensions")
 # options.add_argument('disable-infobars')
