a1a3ea0605034f8c010d0ae69b052258f025cf1f
Emile Houvenaghel <emile.houvenaghel@telenet.be>
2021-04-20 23:07:36 +0200

    Revert "remove executable path geckodriver"
    This reverts commit d5b5a428

--- market_overview.py
+++ market_overview.py
@@ -20,18 +20,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 ###################################################################################################################
+CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/bin/chromedriver')
+GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome-stable')
+
 url = 'https://finviz.com/futures.ashx'
+# options = webdriver.ChromeOptions()
+# options.binary_location = GOOGLE_CHROME_BIN
+# options.add_argument("--headless")
+# options.add_argument("--window-size=1920,1080")
+# options.add_argument('--disable-gpu')
+# options.add_argument('--no-sandbox')
+# options.add_argument("--disable-extensions")
+# options.add_argument('disable-infobars')
 
-CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
-
-chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
-options = webdriver.ChromeOptions()
-options.binary_location = chrome_bin
-options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
-options.add_argument('headless')
-options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+driver = webdriver.Firefox()
+driver.set_window_size(1200, 600)
 driver.get(url)
 driver.maximize_window()
 
